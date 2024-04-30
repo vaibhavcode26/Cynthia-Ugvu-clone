c5f47f9c62b228edac3a4c44172dbf43c996790b
vaibhavcode26 <112636481+vaibhavcode26@users.noreply.github.com>
2024-05-01 02:02:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+ function mousemove(){
+    window.addEventListener('mousemove' , function(dets){
+        document.querySelector('#minicircle').style.transform = `translate(${dets.clientX}px , ${dets.clientY}px)`
+    })
+ }
+ mousemove()
+ function firstpage(){
+    var tl = gsap.timeline();
+    tl.from("#nav" , {
+        y : '-10', 
+        opacity : 0,
+        duration : 1,
+        ease : Expo.easeInOut
+    })
+    tl.to(".boundingelem" , {
+        y : '0', 
+        duration : 1.5,
+        ease : Expo.easeInOut,
+        stagger : .2,
+        delay : -1
+    })
+    tl.from("#footer" , {
+        y : '-10', 
+        opacity : 0,
+        duration : 1.5,
+        delay : -1,
+        ease : Expo.easeInOut
+    })
+ }
+ firstpage()
+document.querySelectorAll(".elem").forEach(function (elem) {
+    var rotate = 0;
+    var diffrot = 0;
+  
+    elem.addEventListener("mouseleave", function (dets) {
+      gsap.to(elem.querySelector("img"), {
+        opacity: 0,
+        ease: Power3,
+        duration: 0.5,
+      });
+    });
+  
+    elem.addEventListener("mousemove", function (dets) {
+      var diff = dets.clientY - elem.getBoundingClientRect().top;
+      diffrot = dets.clientX - rotate;
+      rotate = dets.clientX;
+      gsap.to(elem.querySelector("img"), {
+        opacity: 1,
+        ease: Power3,
+        top: diff,
+        left: dets.clientX,
+        rotate: gsap.utils.clamp(-20, 20, diffrot * 0.5),
+      });
+    });
+  });
